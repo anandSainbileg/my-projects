521370eae6415e33d48d0ffa7698021006c8b99f
Anand Sainbileg <93631431+newlearner69@users.noreply.github.com>
2022-10-01 13:42:29 +0100

    Update OnlineBookStore.java

--- OnlineBookStore.java
+++ OnlineBookStore.java
@@ -130,3 +130,138 @@ public class OnlineBookStore
     	System.out.println("Insufficient amount of money to buy a book ");
     }
 }
+public class Book
+{
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private int publishYear;
+    private int quantity;
+    private double price;
+	
+    public Book()
+    {
+		
+    }
+	
+    public Book(String new_isbn, String new_title, String new_author, String new_publisher, int new_publishYear, int new_quantity, double new_price)
+    {
+    isbn = new_isbn;
+	title = new_title;
+	author = new_author;
+	publisher = new_publisher;
+	publishYear = new_publishYear;
+	quantity = new_quantity;
+	price = new_price;
+    }
+	
+    public int getPublishYear()
+    {
+	return publishYear;
+    }
+
+    public void setPublishYear(int new_publishYear)
+    {
+	publishYear = new_publishYear;
+    }
+
+    public int getQuantity()
+    {
+	return quantity;
+    }
+
+    public void setQuantity(int new_quantity)
+    {
+	quantity = new_quantity;
+    }
+
+    public double getPrice()
+    {
+	return price;
+    }
+
+    public void setPrice(double new_price)
+    {
+	price = new_price;
+    }
+
+    public String getIsbn()
+    {
+	return isbn;
+    }
+
+   public void setIsbn(String new_isbn)
+   {
+	isbn = new_isbn;
+   }
+
+   public String getTitle()
+   {
+        return title;
+   }
+
+   public void setTitle(String new_title)
+   {
+	title = new_title;
+   }
+
+   public String getAuthor()
+   {
+	return author;
+   }
+
+   public void setAuthor(String new_author)
+   {
+	author = new_author;
+   }
+
+   public String getPublisher()
+   {
+	return publisher;
+   }
+
+   public void setPublisher(String new_publisher)
+   {
+	publisher = new_publisher;
+   }
+
+   @Override
+   public String toString()
+   {
+	return "isbn: " + isbn + ", Title: " + title + ", Author: " + author + ", Publisher: " + publisher + ", PublishYear: " + publishYear + ", Quantity: " + quantity + ", Price: " + price + "\n\n";
+   }
+	
+}
+
+public class ChargeCard
+{
+	
+    private double funds;
+	
+    public ChargeCard()
+    {
+	funds = 0.0;
+    }
+	
+    public void topUpFunds(double new_funds)
+    {
+	funds += new_funds;
+    }
+	
+    public void removeFunds(double new_funds)
+    {
+	funds -= new_funds;
+    }
+
+    public double getFunds()
+    {
+	return funds;
+    }
+
+    public void setFunds(double new_funds)
+    {
+	funds = new_funds;
+    }
+}
+
